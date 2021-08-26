1df1c7bd42e623172292e262c0b4b769bee626b1
narumincho <narumincho.starfy@gmail.com>
2021-08-26 22:04:59 +0900

    add kamausagi

---
+++ main.tsx
@@ -0,0 +1,53 @@
+import * as React from "react";
+import * as ReactDom from "react-dom";
+import * as kamausagi from "./kamausagi.png";
+import { css } from "@emotion/css";
+
+const App = (props: { count: number }): React.ReactElement => {
+  return (
+    <div
+      className={css({
+        display: "grid",
+        gap: 8,
+        gridTemplateColumns: "1fr 1fr 1fr",
+      })}
+    >
+      {Array.from({ length: 20 }, (_, i) => i).map((item) => (
+        <Card key={item} text={"記事" + item.toString()} />
+      ))}
+    </div>
+  );
+};
+
+const Card = (props: { text: string }): React.ReactElement => {
+  return (
+    <div
+      className={css({
+        backgroundColor: "red",
+        padding: 8,
+        ":hover": {
+          backgroundColor: "pink",
+        },
+        width: 256,
+        height: 128,
+      })}
+    >
+      <img
+        className={css({
+          width: 256,
+          height: 100,
+          objectFit: "contain",
+          display: "block",
+        })}
+        src={kamausagi}
+        alt="かまうさぎ"
+      />
+      {props.text}
+    </div>
+  );
+};
+
+const entryElement = document.createElement("div");
+document.body.append(entryElement);
+
+ReactDom.render(<App count={2} />, entryElement);
